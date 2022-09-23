5814d254b3d35a23340eb227034b4b02c22189e4
b7urquieta <109691033+b7urquieta@users.noreply.github.com>
2022-09-23 02:23:20 -0500

    javascript contenido articulos tienda

---
+++ assets/js/dataTienda.js
@@ -0,0 +1,71 @@
+let shopItemsData = [
+    {
+        id: "uno",
+        name: "Mueble de belleza profesional con espejo",
+        price: 20000,
+        desc: "Haz relucir tu salón de belleza con este hermoso mueble con espejo, ideal para realizar peinados, maquillaje y tratamientos de skin care.",
+        img: "assets/img/tienda/maquillaje.avif"
+        
+    },
+    {
+        id: "dos",
+        name: "Escritorio gamer",
+        price: 6000,
+        desc: "Escritorio ideal para embellecer tu espacio de trabajo, estudio o relajación. Si practicas eSports este escritorio será tu aliado ideal para crear tu mejor set-up<br>*No incluye accesorios.",
+        img: "assets/img/tienda/escritorio-gamer.jpg"
+        
+    },
+    {
+        id: "tres",
+        name: "Mueble recepción",
+        price: 12000,
+        desc: "Recibe a tus clientes en tu consultorio, clínica y/o negocio con este hermoso mueble de madera con repisas ideales para la demostración de productos.",
+        img: "assets/img/tienda/mueble-recepcion.jpg"
+        
+    },
+    {
+        id: "cuatro",
+        name: "Juego de caballetes",
+        price: 4000,
+        desc: "Caballetes de diferentes medidas adaptados, ideales para hacer resaltar tus cuadros, así como practicar pintura en tus ratos libres.<br>*Contiene 4 piezas.",
+        img: "assets/img/tienda/caballetes.jpg"
+    },
+    {
+        id: "cinco",
+        name: "Maceteros",
+        price: 6500,
+        desc: "Hermosos maceteros ideales para empezar tu propio huerto urbano y decorar tu casa con hermosas plantas y brindar ese toque sustentable a tu hogar",
+        img: "assets/img/tienda/maceteros.jpg"
+    },
+    {
+        id: "seis",
+        name: "Cocina",
+        price: 20000,
+        desc: "Hermosa cocina integral realizada con madera importada de cedro, conocida por su alta durabilidad, con recubrimiento impermeable de alta densidad y puertas y cajones abatibles con soft-touch",
+        img: "assets/img/tienda/cocina.jpeg"
+        
+    },
+    {
+        id: "siete",
+        name: "Comedor",
+        price: 12500,
+        desc: "Comedor rectangular con 6 sillas de respaldo curvo y alto",
+        img: "assets/img/tienda/comedor-2.avif"
+    },
+    {
+        id: "ocho",
+        name: "Utensilios cocina",
+        price: 1200,
+        desc: "Kit de utensilios premium, tallados a mano artesanalmente. Resistentes a líquidos y altas temperaturas. Ideales para el uso cotidiano.",
+        img: "assets/img/tienda/utensilios-cocina.jpg"
+    },
+    {
+        
+        id: "nueve",
+        name: "Bar",
+        price: 10000,
+        desc: "Recibe a todos tus invitados y sé el mejor anfitrión de tus eventos sociales, con esta mesa, bar de 8 bancos y recubrimiento impermeable para que derramar tu bebida, jamás sea un problema.",
+        img: "assets/img/tienda/bar.jpg"
+    }
+
+    ];
